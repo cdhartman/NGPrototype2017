a292a684aa0ce25aa34c41ccbe07976588dab666
cdhartman <hartman.clint@mayo.edu>
2016-05-06 08:49:28 -0500

    Added files via upload

--- js/modExtras.js
+++ js/modExtras.js
@@ -284,8 +284,8 @@ function onHoverDiv(javaScriptAction, selectedDiv, direction, videoFlag, row, co
 	
 	// if (column === 1) {alert('column=1')}
 	if (column === 1 && lastKeyPressed === 37 && javaScriptAction === 'onBlur') {
-		//alert('previousKeyPressed=' + previousKeyPressed + ', lastKeyPressed=' + lastKeyPressed + ', ' + 'column=' + column + ', javaScriptAction=' + javaScriptAction +
-		// ', previousColumnSelected=' + previousColumnSelected + ', lastColumnSelected=' + lastColumnSelected + ', leftNavBarWidth=' + $('#navBarLeft').width());
+		alert('previousKeyPressed=' + previousKeyPressed + ', lastKeyPressed=' + lastKeyPressed + ', ' + 'column=' + column + ', javaScriptAction=' + javaScriptAction +
+		', previousColumnSelected=' + previousColumnSelected + ', lastColumnSelected=' + lastColumnSelected + ', leftNavBarWidth=' + $('#navBarLeft').width());
 		if ( $('#navBarLeft').width() === 45) {
 			navBarLeftFunction('expand');
 			$('#title').trigger('click');
