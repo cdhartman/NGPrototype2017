dd052abc448f70ca06f421b9714c9bd9b9aeb641
cdhartman <hartman.clint@mayo.edu>
2016-05-23 13:44:33 -0500

    Add files via upload

---
+++ js/mci/mciIDLookup.js
@@ -0,0 +1,30 @@
+function mciVideoLookup(id) {
+	"use strict";
+	
+	var constructURL = '',suffixURL = '';
+	
+	if (isPhoneGap()) {
+		suffixURL = 'playlist.m3u8';
+		constructURL = 'playVideo(\'' + mciVideoURLLookup(id) + '/' + suffixURL + ')\'';
+	} else {
+		// suffixURL = 'manifest.f4m';
+		constructURL = 'window.location.href=\'' + mciVideoURLLookup(id) + '\'';
+	}
+	
+	suffixURL = 'playlist.m3u8';
+		constructURL = 'playVideo(\'' + mciVideoURLLookup(id) + '/' + suffixURL + '\')';
+	
+	return constructURL;
+}
+
+function mciVideoURLLookup(id) {
+	"use strict";
+	switch (id) {
+
+	case '1000': return 'http://roqiww002a.mayo.edu:1935/vod/_definst_/library/content/r71v1/entry/data/561/245/0_1jpv6e0i_0_95qjmzw3_2.mp4';
+	case '1001': return 'http://roqiww002a.mayo.edu:1935/vod/_definst_/library/content/r71v1/entry/data/543/17/0_sbb90mjz_0_9ka2nbvq_2.mp4';
+
+		default:
+			return 'http://roqiww002a.mayo.edu:1935/vod/_definst_/library/content/r71v1/entry/data/561/245/0_1jpv6e0i_0_95qjmzw3_2.mp4';
+	}
+}
