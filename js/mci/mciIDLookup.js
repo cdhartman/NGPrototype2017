7b7cae90f359bd662e2a5730a6264ad7a49068c2
cdhartman <hartman.clint@mayo.edu>
2016-05-23 15:45:06 -0500

    Add files via upload

--- js/mci/mciIDLookup.js
+++ js/mci/mciIDLookup.js
@@ -23,7 +23,13 @@ function mciVideoURLLookup(id) {
 
 	case '1000': return 'http://roqiww002a.mayo.edu:1935/vod/_definst_/library/content/r71v1/entry/data/561/245/0_1jpv6e0i_0_95qjmzw3_2.mp4';
 	case '1001': return 'http://roqiww002a.mayo.edu:1935/vod/_definst_/library/content/r71v1/entry/data/543/17/0_sbb90mjz_0_9ka2nbvq_2.mp4';
-
+	case '1002': return 'http://roqiww002a.mayo.edu:1935/vod/_definst_/library/content/r71v1/entry/data/543/17/0_sbb90mjz_0_9ka2nbvq_2.mp4';
+	case '1003': return 'http://roqiww002a.mayo.edu:1935/vod/_definst_/library/content/r71v1/entry/data/543/17/0_sbb90mjz_0_9ka2nbvq_2.mp4';
+	case '1004': return 'http://roqiww002a.mayo.edu:1935/vod/_definst_/library/content/r71v1/entry/data/543/17/0_sbb90mjz_0_9ka2nbvq_2.mp4';
+	case '1005': return 'http://roqiww002a.mayo.edu:1935/vod/_definst_/library/content/r71v1/entry/data/543/17/0_sbb90mjz_0_9ka2nbvq_2.mp4';
+	case '1006': return 'http://roqiww002a.mayo.edu:1935/vod/_definst_/library/content/r71v1/entry/data/543/17/0_sbb90mjz_0_9ka2nbvq_2.mp4';
+	
+	
 		default:
 			return 'http://roqiww002a.mayo.edu:1935/vod/_definst_/library/content/r71v1/entry/data/561/245/0_1jpv6e0i_0_95qjmzw3_2.mp4';
 	}
